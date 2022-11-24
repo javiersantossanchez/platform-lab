0e6374bf2149c901e673fd481b775267fbc87d9c
Javier Santos <javier.david.santos@gmail.com>
2022-11-23 22:56:20 -0500

    Create column to storage relationship with users and add comments to database object

--- credential.manager.service/src/main/resources/db/patch/V1_0_0__init.sql
+++ credential.manager.service/src/main/resources/db/patch/V1_0_0__init.sql
@@ -1,6 +1,6 @@
-
 CREATE TABLE IF NOT EXISTS user_password_credential (
   user_password_credential_id uuid,
+  user_id uuid NOT NULL,
   password VARCHAR(50) NOT NULL,
   user_name VARCHAR(100) NOT NULL,
   credential_name VARCHAR(100) NOT NULL,
@@ -8,11 +8,9 @@ CREATE TABLE IF NOT EXISTS user_password_credential (
   PRIMARY KEY ( user_password_credential_id ),
   CONSTRAINT user_name_unique UNIQUE (user_name)
 );
-
-
-CREATE TABLE IF NOT EXISTS user_password_credential_user (
-  user_password_credential_id uuid NOT NULL,
-  user_id uuid NOT NULL,
-  PRIMARY KEY ( user_password_credential_id,user_id ),
-  FOREIGN KEY ( user_password_credential_id ) REFERENCES user_password_credential (user_password_credential_id)
-);
+COMMENT ON TABLE user_password_credential IS 'information for credential of user and password type';
+COMMENT ON COLUMN user_password_credential.user_password_credential_id IS 'primary key, identify every record on the table';
+COMMENT ON COLUMN user_password_credential.password IS 'Passphrase for the credential';
+COMMENT ON COLUMN user_password_credential.user_name IS 'User name for the credential';
+COMMENT ON COLUMN user_password_credential.credential_name IS 'name to identify the credential';
+COMMENT ON COLUMN user_password_credential.user_id IS 'user id owner of credential';
