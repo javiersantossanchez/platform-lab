ef7e5369e6140abf7ce312e47f845c9b0f62d93a
Javier Santos <javier.david.santos@gmail.com>
2022-11-24 16:45:25 -0500

    Improving code

--- credential.manager.service/src/main/java/com/platform/general/microservice/web/credential/adapters/postgresql/WebCredentialEntity.java
+++ credential.manager.service/src/main/java/com/platform/general/microservice/web/credential/adapters/postgresql/WebCredentialEntity.java
@@ -2,14 +2,18 @@ package com.platform.general.microservice.web.credential.adapters.postgresql;
 
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "user_password_credential")
-@Data
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @NoArgsConstructor
@@ -35,5 +39,16 @@ public class WebCredentialEntity {
     @Column(name = "creation_time",updatable = false,insertable = true,nullable = false)
     private LocalDateTime  creationTime;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        WebCredentialEntity that = (WebCredentialEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
